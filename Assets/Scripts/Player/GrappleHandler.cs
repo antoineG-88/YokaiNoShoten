7ce89c066dea3e15a2ceaef3af5a5087c985e3dd
Antoine <grugeon.antoine@gmail.com>
2020-07-08 17:17:14 +0200

    Basic movement added

---
+++ Assets/Scripts/Player/GrappleHandler.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleHandler : MonoBehaviour
+{
+    public float maxGrappleRange;
+    public float aimAssistAngle;
+    public int aimAssistRaycastNumber;
+
+    public LayerMask ringMask;
+
+    public GameObject grappleCursor;
+    public GameObject grappleSelectIndicator;
+
+    private Vector2 aimDirection;
+    private Ring selectedRing;
+    private bool isAiming;
+    private bool isAttached;
+    private GameObject closestRing;
+    private float aimAssistSubAngle;
+    private float aimAssistFirstAngle;
+
+    void Start()
+    {
+        aimAssistFirstAngle = -aimAssistAngle / 2;
+        aimAssistSubAngle = aimAssistAngle / (aimAssistRaycastNumber - 1);
+        closestRing = null;
+     }
+
+    void Update()
+    {
+        SelectHook();
+    }
+
+    private void SelectHook()
+    {
+        aimDirection = new Vector2(Input.GetAxis("RightStickH"), -Input.GetAxis("RightStickV"));
+        if(!isAiming && aimDirection.magnitude > 0.1f)
+        {
+            isAiming = true;
+        }
+        else if (isAiming && aimDirection.magnitude <= 0.1f)
+        {
+            isAiming = false;
+        }
+
+        if(isAiming)
+        {
+            grappleCursor.SetActive(true);
+            aimDirection.Normalize();
+            grappleCursor.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Vector2.SignedAngle(Vector2.up, aimDirection)));
+
+
+            RaycastHit2D hit;
+            float minAngleFound = aimAssistAngle;
+            closestRing = null;
+            for (int i = 0; i < aimAssistRaycastNumber; i++)
+            {
+                float relativeAngle = aimAssistFirstAngle + aimAssistSubAngle * i;
+                float angledDirection = (Mathf.Atan2(aimDirection.y, aimDirection.x) * 180 / Mathf.PI - 90) + relativeAngle;
+                Vector2 direction = new Vector2(Mathf.Cos((angledDirection + 90) * Mathf.PI / 180), Mathf.Sin((angledDirection + 90) * Mathf.PI / 180));
+                hit = Physics2D.Raycast(transform.position, direction, maxGrappleRange, ringMask);
+                if (hit && closestRing != hit.collider.gameObject && Vector2.Angle(direction, aimDirection) < minAngleFound)
+                {
+                    closestRing = hit.collider.gameObject;
+                    minAngleFound = Vector2.Angle(direction, aimDirection);
+                }
+            }
+        }
+        else
+        {
+            closestRing = null;
+            grappleCursor.SetActive(false);
+        }
+
+
+        if(closestRing != null)
+        {
+            grappleSelectIndicator.SetActive(true);
+            grappleSelectIndicator.transform.position = closestRing.transform.position;
+            if(Input.GetAxisRaw("RightTrigger") == 1)
+            {
+                Attach();
+            }
+        }
+    }
+
+
+    private void Attach()
+    {
+        isAttached = true;
+
+    }
+}
---
+++ Assets/Scripts/Player/MovementHandler.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementHandler : MonoBehaviour
+{
+    public float walkingMaxSpeed;
+    public float walkingMinSpeed;
+    public float walkingAcceleration;
+    public float airAcceleration;
+    public float jumpForce;
+    [Space]
+    [Header("References")]
+    public Collider2D feetCollider;
+
+    private float horizontalTargetSpeed;
+    private float horizontalCurrentSpeed;
+    private float currentAcceleration;
+    [HideInInspector] bool isGrounded;
+
+    private Rigidbody2D rb;
+    private ContactFilter2D groundFilter;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        horizontalCurrentSpeed = 0;
+        isGrounded = false;
+        groundFilter.SetLayerMask(LayerMask.GetMask("Wall"));
+        groundFilter.useTriggers = true;
+    }
+
+    void Update()
+    {
+        UpdateMovementSpeed();
+        if(Input.GetButton("AButton") && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        isGrounded = IsOnGround();
+    }
+
+    private void UpdateMovementSpeed()
+    {
+        currentAcceleration = isGrounded ? walkingAcceleration : airAcceleration;
+
+        if (Mathf.Abs(Input.GetAxis("LeftStickH") * walkingMaxSpeed) > walkingMinSpeed)
+        {
+            horizontalTargetSpeed = Input.GetAxis("LeftStickH") * walkingMaxSpeed;
+
+            horizontalCurrentSpeed += Mathf.Sign(horizontalTargetSpeed - horizontalCurrentSpeed) * currentAcceleration * Time.deltaTime;
+
+            Mathf.Clamp(horizontalCurrentSpeed, -horizontalTargetSpeed, horizontalTargetSpeed);
+        }
+        else
+        {
+            if(Mathf.Abs(horizontalCurrentSpeed) > currentAcceleration * Time.deltaTime)
+            {
+                horizontalCurrentSpeed -= Mathf.Sign(horizontalCurrentSpeed) * currentAcceleration * Time.deltaTime;
+            }
+            else
+            {
+                horizontalCurrentSpeed = 0;
+            }
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(horizontalCurrentSpeed, rb.velocity.y);
+    }
+
+    private bool IsOnGround()
+    {
+        List<Collider2D> colliders = new List<Collider2D>();
+        Physics2D.OverlapCollider(feetCollider, groundFilter, colliders);
+        if(colliders.Count > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+}
