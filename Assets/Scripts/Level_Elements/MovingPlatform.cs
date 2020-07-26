e3a9a6f27262d2d10660a3a1991d080d02991d36
Manon Ghignoni <manon.ghignoni@gmail.com>
2020-07-26 19:07:32 +0200

    Début elements interactifs

--- Assets/Scripts/Level_Elements/MovingPlatform.cs
+++ Assets/Scripts/Level_Elements/MovingPlatform.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Ring platformRing;
     void Start()
     {
         
---
+++ Assets/Scripts/Level_Elements/Sqwasher.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sqwasher : MonoBehaviour
+{
+    public float upSpeed;
+    public float downSpeed;
+    public Vector2 direction;
+    public float timerDown;
+    public float timerUp;
+    public float timerWait;
+    private float timer;
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < timerDown)
+        {
+            MoveDown();
+        }
+        else if (timer < timerDown + timerWait)
+        {
+
+        }
+        else
+        {
+            MoveUp();
+        }
+
+        if (timer > timerDown + timerWait + timerUp)
+        {
+            timer = 0;
+        }
+
+
+    }
+    private void MoveUp()
+    {
+        transform.position += (Vector3)(direction * upSpeed * Time.fixedDeltaTime);
+    }
+
+    private void MoveDown()
+    {
+        transform.position -= (Vector3)(direction * downSpeed * Time.fixedDeltaTime);
+    }
+}
