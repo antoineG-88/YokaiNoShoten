9db8d4b893efb5db863da297bfc033729986ec53
Antoine <a.grugeon@rubika-edu.com>
2021-07-20 11:24:58 +0200

    ElementhPath improved

--- Assets/Scripts/Level_Elements/ElementPath.cs
+++ Assets/Scripts/Level_Elements/ElementPath.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class ElementPath : MonoBehaviour
 {
     public Transform[] pathPositions;
+    public bool useSpeed;
     public float speed;
+    public float timeToCompleteFullLoop;
     public bool isBackAndForth;
     public bool endLoopByTp;
     [Range(0.0f, 100.0f)] public float startProgression;
-    public float pauseTime;
     public Switch connectedSwitch;
-
-    private int currentTargetPositon;
-    private int previousTargetPosition;
-    private bool isReturning;
 
     private Vector2 currentDirection;
     private Vector2[] pathVectors;
     private float pathLength;
     Rigidbody2D rb;
     private bool isActive;
+    private float currentSpeed;
+    private float loopTime;
+    private float currentProgression;
+    private float progressionSpeed;
+    private int currentTargetPositonIndex;
+    private int previousTargetPositionIndex;
+    private bool isGoingBack;
 
     void Start()
     {
+        isGoingBack = false;
         isActive = true;
         rb = GetComponent<Rigidbody2D>();
         pathVectors = new Vector2[pathPositions.Length];
@@ -48,39 +53,29 @@ public class ElementPath : MonoBehaviour
             {
                 pathLength += pathVectors[i].magnitude;
             }
-            else if(isBackAndForth)
-            {
-                pathLength *= 2;
-            }
         }
 
-        if(startProgression != 0 && startProgression != 100 && !isBackAndForth)
+        if (useSpeed)
         {
-            float startDistance = startProgression / 100 * pathLength;
-            float distance = 0;
-            if (!isBackAndForth)
-            {
-                int i = 0;
-                while (distance != startDistance && i < pathPositions.Length)
-                {
-                    if (distance + pathVectors[i].magnitude < startDistance)
-                    {
-                        distance += pathVectors[i].magnitude;
-                    }
-                    else
-                    {
-                        transform.position = Vector2.Lerp(pathPositions[i].position, pathPositions[i + 1 < pathPositions.Length ? i + 1 : 0].position, (startDistance - distance) / pathVectors[i].magnitude);
-                        currentDirection = pathVectors[i].normalized;
-                        currentTargetPositon = i + 1 < pathPositions.Length ? i + 1 : 0;
-                        distance = startDistance;
-                    }
-                    i++;
-                }
-            }
+            currentSpeed = speed;
+            loopTime = speed * pathLength;
         }
         else
         {
-            currentTargetPositon = 1;
+            currentSpeed = pathLength / timeToCompleteFullLoop;
+            loopTime = timeToCompleteFullLoop;
+        }
+
+        progressionSpeed = currentSpeed / pathLength;
+        currentProgression = startProgression / 100;
+
+        if (startProgression != 0 && startProgression != 100)
+        {
+            transform.position = GetPosProgressionInPath(startProgression / 100);
+        }
+        else
+        {
+            currentTargetPositonIndex = 1;
             transform.position = pathPositions[0].position;
             currentDirection = pathPositions[1].position - pathPositions[0].position;
             currentDirection.Normalize();
@@ -93,58 +88,8 @@ public class ElementPath : MonoBehaviour
         UpdateDirection();
     }
 
-
     private void UpdateDirection()
     {
-        if (Vector2.Distance(transform.position, pathPositions[currentTargetPositon].position) < Time.fixedDeltaTime * speed *3)
-        {
-            previousTargetPosition = currentTargetPositon;
-            transform.position = pathPositions[currentTargetPositon].position;
-            
-            if(isReturning == false)
-            {
-                currentTargetPositon += 1;
-            }
-            else 
-            {
-                currentTargetPositon -= 1;
-            }
-
-            if (currentTargetPositon == pathPositions.Length)
-            {
-                if(isBackAndForth == false)
-                {
-                    if(endLoopByTp)
-                    {
-                        currentTargetPositon = 1;
-                        previousTargetPosition = 0;
-                        transform.position = pathPositions[0].position;
-                    }
-                    else
-                    {
-                        currentTargetPositon = 0;
-                    }
-                }
-                else
-                {
-                    currentTargetPositon -= 2;
-                    isReturning = true;
-                }
-
-            }
-
-            if (currentTargetPositon == -1 && isBackAndForth == true)
-            {
-                currentTargetPositon += 2;
-                isReturning = false;
-            }
-
-            currentDirection = pathPositions[currentTargetPositon].position - pathPositions[previousTargetPosition].position;
-            currentDirection.Normalize();
-
-
-        }
-
         if(connectedSwitch != null)
         {
             isActive = connectedSwitch.isOn;
@@ -152,12 +97,123 @@ public class ElementPath : MonoBehaviour
 
         if(isActive)
         {
-            rb.velocity = currentDirection * speed;
+            rb.velocity = currentDirection * currentSpeed;
+            if(!isBackAndForth)
+            {
+                currentProgression += progressionSpeed * Time.fixedDeltaTime;
+
+                if (currentProgression > 1)
+                {
+                    currentProgression -= 1;
+                }
+            }
+            else
+            {
+                currentProgression += progressionSpeed * Time.fixedDeltaTime * (isGoingBack ? -1 : 1);
+
+                if(currentProgression < 0)
+                {
+                    isGoingBack = false;
+                }
+
+                if(currentProgression > 1)
+                {
+                    isGoingBack = true;
+                }
+            }
+
+
+            transform.position = GetPosProgressionInPath(currentProgression);
         }
         else
         {
             rb.velocity = Vector2.zero;
         }
+    }
+
+    private void RefreshDirection()
+    {
+        currentDirection = pathPositions[currentTargetPositonIndex].position - pathPositions[previousTargetPositionIndex].position;
+        currentDirection.Normalize();
+    }
+
+    private Vector2 GetPosProgressionInPath(float progression)
+    {
+        float startDistance = progression * pathLength;
+        float distance = 0;
+        Vector2 pos = Vector2.zero;
+
+
+        if (isBackAndForth)
+        {
+            int i = 0;
+            while (distance != startDistance && i != pathPositions.Length)
+            {
+                if (distance + pathVectors[i].magnitude < startDistance)
+                {
+                    distance += pathVectors[i].magnitude;
+                }
+                else
+                {
+                    pos = Vector2.Lerp(pathPositions[i].position, pathPositions[i + 1 < pathPositions.Length ? i + 1 : 0].position, (startDistance - distance) / pathVectors[i].magnitude);
+                    distance = startDistance;
+
+                    if(isGoingBack)
+                    {
+                        if (currentTargetPositonIndex != i)
+                        {
+                            previousTargetPositionIndex = currentTargetPositonIndex;
+                            currentTargetPositonIndex = i;
+                            RefreshDirection();
+                        }
+                    }
+                    else
+                    {
+                        if (currentTargetPositonIndex != (i + 1 < pathPositions.Length ? i + 1 : i - 1))
+                        {
+                            previousTargetPositionIndex = currentTargetPositonIndex;
+                            currentTargetPositonIndex = i + 1 < pathPositions.Length ? i + 1 : i - 1;
+                            if (endLoopByTp && currentTargetPositonIndex == 1)
+                            {
+                                previousTargetPositionIndex = 0;
+                            }
+                            RefreshDirection();
+                        }
+                    }
+                }
+
+                i++;
+            }
+        }
+        else
+        {
+            int i = 0;
+            while (distance != startDistance && i < pathPositions.Length)
+            {
+                if (distance + pathVectors[i].magnitude < startDistance)
+                {
+                    distance += pathVectors[i].magnitude;
+                }
+                else
+                {
+                    pos = Vector2.Lerp(pathPositions[i].position, pathPositions[i + 1 < pathPositions.Length ? i + 1 : 0].position, (startDistance - distance) / pathVectors[i].magnitude);
+                    distance = startDistance;
+                    if(currentTargetPositonIndex != (i + 1 < pathPositions.Length ? i + 1 : endLoopByTp ? 1 : 0))
+                    {
+                        previousTargetPositionIndex = currentTargetPositonIndex;
+                        currentTargetPositonIndex = i + 1 < pathPositions.Length ? i + 1 : endLoopByTp ? 1 : 0;
+                        if (endLoopByTp && currentTargetPositonIndex == 1)
+                        {
+                            previousTargetPositionIndex = 0;
+                        }
+                        RefreshDirection();
+                    }
+                }
+                i++;
+            }
+        }
+
+        return pos;
     }
 
     private void OnDrawGizmos()
